ea4b3b0fd0117fb3cc4a825e89ffa11a1fa46f57
th3mon <p.szelenberger@gmail.com>
2018-08-22 17:35:14 +0200

    Scrap headers

--- get-wiki-content.js
+++ get-wiki-content.js
@@ -1,7 +1,7 @@
 const clients = require('restify-clients');
 const cheerio = require('cheerio');
 const fs = require('fs');
-const writeStream = fs.createWriteStream('data-from-wiki.html');
+const _ = require('lodash');
 
 const sectionId = 'mwAac';
 const wikiApiUrl = `https://en.wikipedia.org/api/rest_v1/page/html/Marvel_Cinematic_Universe?sections=${sectionId}`;
@@ -11,10 +11,29 @@ client.get('', (err, req, res, obj) => {
   const html = obj[sectionId];
   const $ = cheerio.load(html);
   const table = $('.wikitable');
+  const headers = scrapHeaders(table, $);
 
-  console.log(table.html());
+
+  console.log(headers);
+  writeToFile(table.html());
 });
 
-function writeToFile() {
-  writeStream.write(html);
+function scrapHeaders(table, $) {
+  const headers = [];
+
+  table.find('tr:first-child th').each((i, el) => {
+    const headerContent = $(el).text().replace(/\(s\)/, '');
+
+    headers.push(_.kebabCase(headerContent));
+  });
+
+  return headers;
+}
+
+function writeToFile(content) {
+  const writeStream = fs.createWriteStream('data-from-wiki.html');
+
+  writeStream.write('<table>')
+  writeStream.write(content);
+  writeStream.write('</table>')
 }
