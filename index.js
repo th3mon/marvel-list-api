1c0b1eff338cad894aaf08091972ff27ab317889
th3mon <p.szelenberger@gmail.com>
2018-08-24 02:39:42 +0200

    Add lint script

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const jsonServer = require('json-server');
+const restify = require('restify');
 const cors = require('cors');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+
+const server = restify.createServer();
 const port = 8084;
 
-server.use(cors());
-server.use('/v1', router);
-server.use(router);
+server.name = 'MarvelAPI';
 
-server.listen(port, () =>
-  console.log(`API Server is running at port: http://localhost:${port}`)
-);
+server.pre(restify.plugins.pre.userAgentConnection());
+server.use(cors());
+
+function respond(req, res, next) {
+  res.send(`hello ${req.params.name}`);
+
+  next();
+}
+
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(port, () => (server.url.includes('http://[::]')
+  ? console.log('%s listening at %s', server.name, `http://localhost:${port}`)
+  : console.log('%s listening at %s', server.name, server.url)));
