bdc21a57153182cb58c95a03eb3201145d1f2a46
th3mon <p.szelenberger@gmail.com>
2018-09-10 02:00:14 +0200

    Movies should have image url
    Also I changed "should be a Promise" to "should have then function".

--- components/web-scrapping/scrap-wiki-content.spec.js
+++ components/web-scrapping/scrap-wiki-content.spec.js
@@ -16,7 +16,7 @@ const movies = [
 ];
 
 const scrapMoviesDataMock = jest.fn().mockResolvedValue(movies);
-const imageUrl = '';
+const imageUrl = '/movie-image-url.png';
 const scrapMoviePosterUrlMock = jest.fn().mockResolvedValue(imageUrl);
 const writeToFileMock = jest.fn();
 const wikiContentScrapper = createWikiContentScrapper(
@@ -34,19 +34,33 @@ describe('Scrap Wiki Content', () => {
     expect(typeof wikiContentScrapper).toBe('function');
   });
 
-  it('should be a Promise', () => {
-    expect(wikiContentScrapper() instanceof Promise).toBe(true);
+  it('should have then function', () => {
+    expect(typeof wikiContentScrapper().then).toBe('function');
   });
 
   it('should expected movies length be equal to movies length', done => {
     wikiContentScrapper()
-      .then(expectedMovies => expect(expectedMovies).toHaveLength(movies.length))
+      .then(expectedMovies =>
+        expect(expectedMovies).toHaveLength(movies.length)
+      )
+      .then(done);
+  });
+
+  it('should expected the first movie be equal to the first movie', done => {
+    wikiContentScrapper()
+      .then(expectedMovies => expect(expectedMovies[0]).toEqual(movies[0]))
       .then(done);
   });
 
   it('should use scrapMoviesData', done => {
     wikiContentScrapper()
       .then(() => expect(scrapMoviesDataMock).toHaveBeenCalled())
+      .then(done);
+  });
+
+  it('should movies have image url', done => {
+    wikiContentScrapper()
+      .then(expectedMovies => expect(expectedMovies[0].imageUrl).toBe(imageUrl))
       .then(done);
   });
 });
