2c8195b9bdb369502fd87031c68a3bc16c7855ef
th3mon <p.szelenberger@gmail.com>
2018-09-10 02:51:28 +0200

    Make main script

---
+++ components/web-scrapping/index.js
@@ -0,0 +1,29 @@
+const clients = require('restify-clients');
+const createJsonFileWriter = require('./create-json-file-writer');
+const config = require('../../config');
+const createMovieDataScrapper = require('./create-scrap-movies-data');
+const createMoviePosterUrlScrapper = require('./create-movie-poster-url-scrapper');
+const createWikiContentScrapper = require('./wiki-content-scrapper');
+
+const scrapMoviesData = createMovieDataScrapper(
+  clients.createJsonClient(config.wikiApi.url)
+);
+const scrapMoviePosterUrl = createMoviePosterUrlScrapper(
+  clients.createStringClient(config.wikiApi.url)
+);
+
+function writeToFile(content) {
+  const wikiDataWriter = createJsonFileWriter('data-from-wiki');
+
+  wikiDataWriter(content);
+}
+
+const wikiContentScrapper = createWikiContentScrapper(
+  scrapMoviesData,
+  scrapMoviePosterUrl,
+  writeToFile
+);
+
+module.exports = {
+  scrap: () => wikiContentScrapper()
+};
