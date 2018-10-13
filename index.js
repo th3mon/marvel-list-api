9a55bdfe45803a41efc871111f701e6889e458b4
th3mon <p.szelenberger@gmail.com>
2018-10-14 01:01:19 +0200

    Remove restify sample code

--- index.js
+++ index.js
@@ -8,15 +8,6 @@ const getMovies = require('./components/movies/movies');
 server.pre(restify.plugins.pre.userAgentConnection());
 server.use(cors());
 
-function respond(req, res, next) {
-  res.send(`hello ${req.params.name}`);
-
-  next();
-}
-
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
-
 server.get('/movies', getMovies);
 
 server.listen(config.port, () => (server.url.includes('http://[::]')
